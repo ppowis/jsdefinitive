179e1abc9ebfb91b76fa07f2da34db970728fbe9
Phil Powis <phil@simplypsquared.com>
2013-10-17 06:53:37 -0400

    Added some notes/test code

---
+++ chapter 1/javascript.js
@@ -0,0 +1,13 @@
+// The Global Object
+ var global = this; // Define a global variable to refer to the global Object
+
+ //Strings have properties and methods:
+
+ var s = "hello world";	// A String
+ var word = s.substring(indexOf(" ")+1, s.length); //Use string properties
+
+ var t = "test";  // Start with a string value.
+ t.len = 4; // Set a property on it.
+ var e = t.len;	// Now query the property.
+
+ 
